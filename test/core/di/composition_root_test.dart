5a2afbcec5f216b09dfb4d039536775e68b92570
David Ruiz <davidru85@gmail.com>
2026-06-03 00:50:10 +0200

    test(di): add DI composition-root RED test for GetIt registrations

--- test/core/di/composition_root_test.dart
+++ test/core/di/composition_root_test.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive_ce/hive.dart';
 import 'package:radio_app/core/di/composition_root.dart';
 import 'package:radio_app/data/datasources/audio_playback_data_source.dart';
 import 'package:radio_app/data/datasources/local/local_countries_data_source.dart';
@@ -14,6 +15,9 @@ import 'package:radio_app/data/datasources/remote/remote_countries_data_source.d
 import 'package:radio_app/data/datasources/remote/remote_genres_data_source.dart';
 import 'package:radio_app/data/datasources/remote/remote_playback_url_data_source.dart';
 import 'package:radio_app/data/datasources/remote/remote_station_data_source.dart';
+import 'package:radio_app/data/models/country_hive_model.dart';
+import 'package:radio_app/data/models/genre_hive_model.dart';
+import 'package:radio_app/data/models/station_hive_model.dart';
 import 'package:radio_app/data/repositories/no_op_analytics_repository_impl.dart';
 import 'package:radio_app/domain/repositories/analytics_repository.dart';
 import 'package:radio_app/domain/repositories/audio_player_repository.dart';
@@ -43,6 +47,7 @@ import 'package:radio_app/domain/usecases/track_analytics_event_use_case.dart';
 import 'package:radio_app/domain/usecases/watch_connectivity_use_case.dart';
 import 'package:radio_app/domain/usecases/watch_now_playing_use_case.dart';
 import 'package:radio_app/domain/usecases/watch_player_state_use_case.dart';
+import 'package:radio_app/hive_registrar.g.dart';
 import 'package:radio_app/presentation/blocs/connectivity/connectivity_bloc.dart';
 import 'package:radio_app/presentation/blocs/countries/countries_bloc.dart';
 import 'package:radio_app/presentation/blocs/favorites/favorites_bloc.dart';
@@ -54,13 +59,30 @@ import 'package:radio_app/presentation/blocs/stations/stations_bloc.dart';
 void main() {
   group('configureDependencies', () {
     late GetIt getIt;
+    late Directory tempDir;
 
-    setUp(() {
+    setUp(() async {
       getIt = GetIt.asNewInstance();
+      tempDir = Directory.systemTemp.createTempSync();
+      Hive.init(tempDir.path);
+
+      if (!Hive.isAdapterRegistered(0)) {
+        Hive.registerAdapters();
+      }
+
+      await Hive.openBox<dynamic>('app_settings');
+      await Hive.openBox<StationHiveModel>('favorites');
+      await Hive.openBox<StationHiveModel>('history');
+      await Hive.openBox<GenreHiveModel>('genres');
+      await Hive.openBox<CountryHiveModel>('countries');
     });
 
     tearDown(() async {
       await getIt.reset();
+      await Hive.close();
+      if (tempDir.existsSync()) {
+        tempDir.deleteSync(recursive: true);
+      }
     });
 
     test('registers every data source, repository, use case, and BLoC', () {
